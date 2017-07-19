dab07d1b7febf0dca889f43ce1e14b259f56445a
Mokhamad Ariadi <mokhamad27@gmail.com>
2017-07-19 17:11:23 +0700

    still error perizinan

--- karyawan/views.py
+++ karyawan/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+
+from karyawan.models import Karyawan
 
 # Create your views here.
+
+def profil(request):
+	karyawan = Karyawan.objects.get(id=request.session['karyawan_id'])
+	return render(request, 'profil.html', {"karyawan":karyawan})
--- kehadiran/admin.py
+++ kehadiran/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from kehadiran.models import Kehadiran
+from kehadiran.models import Izin
 
 # Register your models here.
 class KehadiranAdmin (admin.ModelAdmin):
@@ -9,3 +10,12 @@ class KehadiranAdmin (admin.ModelAdmin):
     list_per_page = 25
 
 admin.site.register(Kehadiran, KehadiranAdmin)
+
+class IzinAdmin(admin.ModelAdmin):
+	list_display = ['karyawan', 'jenis_kehadiran', 'waktu_mulai', 'waktu_berhenti', 'disetujui']
+	list_filter = ('jenis_kehadiran', 'disetujui')
+	search_fields = ['alasan']
+	list_per_page = 25
+
+admin.site.register(Izin, IzinAdmin)
+	
